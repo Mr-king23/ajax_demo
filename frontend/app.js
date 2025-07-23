f00a2a9a4a3c03bdbae35bc1254268ffa3213c58
Sakshyam Gyawali <gyawalisakshyam4@gmail.com>
2025-07-23 18:13:03 +0545

    next changes

--- frontend/app.js
+++ frontend/app.js
@@ -13,7 +13,7 @@ form.addEventListener("submit", async (e) => {
 
   try {
     // POST to FastAPI
-    const res = await fetch(`https://ajax-demo-w8ob-id0bn108e-sakshyam-gyawalis-projects.vercel.app/${endpoint}`, {
+    const res = await fetch(`https://ajax-demo-w8ob-id0bn108e-sakshyam-gyawalis-projects.vercel.app${endpoint}`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
